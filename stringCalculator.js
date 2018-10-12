1c02a61d96b77a61e5b1100043140126b2d958c7
unknown <rolandas17@ru.is>
2018-10-12 18:13:30 +0000

    implemented funtionality for single value (string "1" returns 1)

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,10 +1,13 @@
 function add(values)
 {
-    if(values.length == 0)
+    if(values.length === 0)
     {
         return 0;
     }
-    
+    else
+    {
+        return parseInt(values);
+    }
     
 }
 module.exports = add;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,6 @@ const add = require('./stringCalculator');
 it('should return 0 on an empty string', () => {
     expect(add("")).toBe(0);
 })
+it('should return 1 on a string "1"', () => {
+    expect(add("1")).toBe(1);
+})
